f24bb4f38e9e9fe61593cab3f96517a86da2ba63
Prashanna21 <prashnnanewar21@gmail.com>
2023-12-02 22:33:07 +0545

    day 7 2

--- Day 7/app.js
+++ Day 7/app.js
@@ -3,7 +3,15 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head> <title> Susyy K </title></head>')
+    res.write('<body>')
+    res.write("<div> hello </div>")
+    res.write<"/body">
+    res.write('</html>')
+    res.end()
 })
 
 
